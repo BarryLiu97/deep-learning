e25f28812b7c26737ab712ea0e840bf3e0d2a1c2
Barry <3273376560@qq.com>
2022-05-12 01:36:31 +0800

    ConvNet and MNIST classificiation

---
+++ dataset.py
@@ -0,0 +1,30 @@
+from torch.utils.data import Dataset, DataLoader
+
+
+class MNISTDataset(Dataset):
+    
+    def __init__(self, img_dir, annot_path, transform=None, target_transform=None):
+        import pandas as pd
+        
+        self.img_dir = img_dir
+        self.annot = pd.read_csv(annot_path, header=None, sep='\t')
+        self.transform = transform
+        self.target_transform = target_transform
+               
+    def __getitem__(self, idx):
+        import os.path as op
+        import cv2
+        
+        img_path = op.join(self.img_dir, self.annot.iloc[idx, 0])
+        target = self.annot.iloc[idx, 1]
+        img = cv2.imread(img_path)
+        
+        if self.transform:
+            img = self.transform(img)
+        if self.target_transform:
+            target = self.target_transform(target)
+        
+        return img, target
+        
+    def __len__(self):
+        return len(self.annot)
