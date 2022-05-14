1ec092af518a8839fa2c54837925fe627b6659cb
Barry <3273376560@qq.com>
2022-05-14 18:29:37 +0800

    add BN for GenderNet

--- model/gender.py
+++ model/gender.py
@@ -4,10 +4,15 @@ from torch import nn
 
 
 class GenderNet(nn.Module):
-    def __init__(self):
+    def __init__(self, do_bn=True):
         super(GenderNet, self).__init__()
+        self.do_bn = do_bn
+        self.bn = nn.BatchNorm1d(2, eps=1e-5)
+        # self.bn = nn.BatchNorm1d(1, eps=0, affine=False, track_running_stats=False)
         self.fc = torch.nn.Linear(2, 2)
         
     def forward(self, x):
+        if self.do_bn:
+            x = self.bn(x)
         x = self.fc(x)
         return x
